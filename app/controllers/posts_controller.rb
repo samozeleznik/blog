5e6ddfd7104006a964103c62ad4e08d3c0a5832f
Samo <samo.zeleznik5@gmail.com>
2014-02-23 16:27:05 +0100

    restrictions on new posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,15 @@
 class PostsController < ApplicationController
 	def new
+		@post=Post.new
 	end
 
 	def create
 		@post=Post.new(post_params)
-		@post.save
-		redirect_to @post
+		if @post.save
+			redirect_to @post
+		else
+			render 'new'
+		end
 	end
 
 	def show
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,4 @@
 class Post < ActiveRecord::Base
+	validates :title, presence: true,
+                    length: { maximum: 20, minimum: 3 }
 end
