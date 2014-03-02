adad04bcfe014e21e755b6e156c630d68760006e
Samo <samo.zeleznik5@gmail.com>
2014-03-02 19:40:24 +0100

    basic security
    Added basic security login for admin

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-	has_many :comments dependant: :destroy
+	has_many :comments, dependent: :destroy
 	validates :title, presence: true,
                     length: { maximum: 20, minimum: 3 }
 end
