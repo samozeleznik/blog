1e941adf94514fe87aad3688db09c9ae9788d9ab
Samo <samo.zeleznik5@gmail.com>
2014-03-02 19:36:04 +0100

    deleting comment
    Deleting comments (also when deleting posts)

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-	has_many :comments
+	has_many :comments dependant: :destroy
 	validates :title, presence: true,
                     length: { maximum: 20, minimum: 3 }
 end
